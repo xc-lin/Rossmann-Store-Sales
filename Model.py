70f79e40f85d940ca96b54b8aed037957341f8c4
lxc <48900984+xc-lin@users.noreply.github.com>
2021-07-25 16:53:21 +0800

    modify GeneratePlot.py

--- Model.py
+++ Model.py
@@ -213,7 +213,7 @@ def xgboostPredict(x_valid, y_valid, test_data):
 
     submission_df['Sales'] = (np.exp(y_test_hat) - 1) * correction_factor
     submission_df.sort_values('Id', inplace=True)
-    submission_df[['Id', 'Sales']].to_csv('submission2.csv', index=False)
+    submission_df[['Id', 'Sales']].to_csv('submission.csv', index=False)
     print("submission.csv generation is finished...")
     print()
 
